a3bf65e66e0d3bb85bb3a17efb497c9875e9919c
Afaifaifai <131575961+Afaifaifai@users.noreply.github.com>
2025-01-16 07:31:39 +0800

    加入ip檢查 以及修改nginx.conf

--- settings.go
+++ settings.go
@@ -1,6 +1,7 @@
 package main
 
-const LISTEN_PORT string = "35.78.125.91:25566"
+const LISTEN_PORT string = "0.0.0.0:25566"
+const ALLOW_IP string = "35.78.125.91"
 
 const AUTH_PASSWORD string = "icanlab"
 const SCRIPT_PATH string = "./whitelist_add.sh"
